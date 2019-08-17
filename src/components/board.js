a513a31c85f8870493261a18709384a6aed9e409
Gleb Dolgushin <dolgushing@gmail.com>
2019-08-18 01:03:21 +0300

    правки

--- src/components/board.js
+++ src/components/board.js
@@ -3,20 +3,27 @@ import {getCardEditMarkup} from "./card-edit";
 import {getLoadMoreButtonMarkup} from './load-more-button.js';
 import {getSortingMarkup} from './sorting.js';
 
-const getCards = (numberOfCards) => {
-  let cards = ``;
-  for (let i = 0; i < numberOfCards; i++) {
-    cards += getCardMarkup();
-  }
-  return cards;
+// Соединяем шаблоны карточек
+// const getCards = (numberOfCards) => {
+//   let cards = ``;
+//   for (let i = 0; i < numberOfCards; i++) {
+//     cards += getCardMarkup();
+//   }
+//   return cards;
+// };
+
+const getCards = (items, getTemplate) => {
+  const templates = items.map((item) => getTemplate(item));
+  return templates.join(``).trim();
 };
 
-export const getBoard = (numberOfCards) =>
+// Заполняем доску
+export const getBoard = (cards) =>
   `<section class="board container">
 ${getSortingMarkup()}
 <div class="board__tasks">
 ${getCardEditMarkup()}
-${getCards(numberOfCards)}
+${getCards(cards.slice(1), getCardMarkup)}
 </div>
 ${getLoadMoreButtonMarkup()}
 </section>`;
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import {getMenuMarkup} from './components/site-menu.js';
 import {getSearchMarkup} from './components/search.js';
 import {getFiltersMarkup} from './components/filter.js';
 import {getBoard} from './components/board.js';
+import {getCardsData} from "./data";
 
 const NUMBER_OF_CARDS = 3;
 
@@ -12,7 +13,20 @@ const addBlock = (container, markup) => {
 
 const main = document.querySelector(`.main`);
 const controlSection = main.querySelector(`.main__control`);
+const cards = getCardsData(NUMBER_OF_CARDS);
 addBlock(controlSection, getMenuMarkup());
 addBlock(main, getSearchMarkup());
 addBlock(main, getFiltersMarkup());
-addBlock(main, getBoard(NUMBER_OF_CARDS));
+addBlock(main, getBoard(cards.slice(0, 8)));
+
+// const tasksContainer = document.querySelector(`.board__tasks`);
+
+// const renderTasks = (container, numberOfCards) => {
+//   container.insertAdjacentHTML(`beforeend`, new Array(numberOfCards)
+//     .fill(``)
+//     .map(getCardData)
+//     .map(getCardMarkup)
+//     .join(``));
+// };
+
+// renderTasks(tasksContainer, NUMBER_OF_CARDS);
